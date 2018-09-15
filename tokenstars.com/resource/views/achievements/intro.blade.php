54ecaff285d7a3dcd385ca8023ca66ae420f403c
Mashkovsky Andrey <mashkovsky_ab@tokenstars.com>
2018-09-15 11:54:01 +0700

    intro

---
+++ tokenstars.com/resource/views/achievements/intro.blade.php
@@ -0,0 +1,62 @@
+<div class="jumbotron jumbotron-intro jumbotron-fluid text-center text-white bg-blue-darker position-relative jumbotron-achievements d-flex align-items-center">
+    <div class="jumbotron-image bg-achievements container"></div>
+    <div class="container jumbotron-container">
+        <h1 class="text-uppercase jumbotron-title">
+            <small class="jumbotron-sup-title text-pink d-block font-weight-bold">Tokenstars</small>
+            Achievements
+        </h1>
+        <div class="row feature-row flex-nowrap mt-5 pt-1">
+
+            <div class="col feature-col d-flex">
+                <div class="feature-item w-100 mx-auto">
+                    <div class="icon icon-feature-auction feature-item-icon mb-2">
+                        <svg viewBox="0 0 1 1"><use xlink:href="/images/icons.svg#feature-auction"></use></svg>
+                    </div>
+                    <h4 class="mb-2 feature-item-title">Auctions are&nbsp;live</h4>
+                    <p class="mb-0 feature-item-descr">17&nbsp;Charity auctions completed on&nbsp;the TokenStars platform</p>
+                </div>
+            </div>
+
+            <div class="col feature-col d-flex">
+                <div class="feature-item w-100 mx-auto">
+                    <div class="icon icon-feature-voting feature-item-icon mb-2">
+                        <svg viewBox="0 0 1 1"><use xlink:href="/images/icons.svg#feature-voting"></use></svg>
+                    </div>
+                    <h4 class="mb-2 feature-item-title">Voting and betting are&nbsp;live</h4>
+                    <p class="mb-0 feature-item-descr">44000 predictions made already, new contest every&nbsp;month</p>
+                </div>
+            </div>
+
+            <div class="col feature-col d-flex">
+                <div class="feature-item w-100 mx-auto">
+                    <div class="icon icon-feature-celebs feature-item-icon mb-2">
+                        <svg viewBox="0 0 1 1"><use xlink:href="/images/icons.svg#feature-celebs"></use></svg>
+                    </div>
+                    <h4 class="mb-2 feature-item-title">18&nbsp;Celebrities on&nbsp;the platform</h4>
+                    <p class="mb-0 feature-item-descr">18&nbsp;celebrities already work, advise and participate in&nbsp;the activities</p>
+                </div>
+            </div>
+
+            <div class="col feature-col d-flex">
+                <div class="feature-item w-100 mx-auto">
+                    <div class="icon icon-feature-community feature-item-icon mb-2">
+                        <svg viewBox="0 0 1 1"><use xlink:href="/images/icons.svg#feature-community"></use></svg>
+                    </div>
+                    <h4 class="mb-2 feature-item-title">Big community</h4>
+                    <p class="mb-0 feature-item-descr">More than 10.000 users actively participate in&nbsp;the platform</p>
+                </div>
+            </div>
+
+            <div class="col feature-col d-flex">
+                <div class="feature-item w-100 mx-auto">
+                    <div class="icon icon-feature-scouting feature-item-icon mb-2">
+                        <svg viewBox="0 0 1 1"><use xlink:href="/images/icons.svg#feature-scouting"></use></svg>
+                    </div>
+                    <h4 class="mb-2 feature-item-title">Scouting module</h4>
+                    <p class="mb-0 feature-item-descr">Coming soon</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
