98cfa454a92aa89a897671a512050b79cac44ac5
Mashkovsky Andrey <mashkovsky_ab@mail.ru>
2018-09-19 11:54:49 +0700

    sections views

---
+++ tokenstars.com/resource/views/achievements/sections.blade.php
@@ -0,0 +1,193 @@
+<section>
+    <h3 class="text-uppercase mb-0 text-blue-gray-light">Predictions Module</h3>
+    <div class="row case-row pb-4">
+
+        <div class="col col-4 case-col d-flex">
+            <div class="case-item media my-4 position-relative">
+                <img class="mr-4 case-item-image" src="../images/pdf-doc.svg" alt="" width="120" height="160">
+                <div class="media-body">
+                    <h4 class="mb-3 case-item-title"><a class="fill-area-link" href="">TokenStars World Cup 2018 Contest</a></h4>
+                    <ul class="list-unstyled list-base mb-0 case-item-list list-base-shift">
+
+                        <li class="my-2">10,053 predictions</li>
+
+                        <li class="my-2">$38,034 won</li>
+
+                        <li class="my-2">87 games played</li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col col-4 case-col d-flex">
+            <div class="case-item media my-4 position-relative">
+                <img class="mr-4 case-item-image" src="../images/pdf-doc.svg" alt="" width="120" height="160">
+                <div class="media-body">
+                    <h4 class="mb-3 case-item-title"><a class="fill-area-link" href="">TokenStars US Open Predictions Contest</a></h4>
+                    <ul class="list-unstyled list-base mb-0 case-item-list list-base-shift">
+
+                        <li class="my-2">4,323 predictions</li>
+
+                        <li class="my-2">10 428 TEAM tokens won</li>
+
+                        <li class="my-2">13 games played</li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col col-4 case-col d-flex">
+            <div class="case-item media my-4 position-relative">
+                <img class="mr-4 case-item-image" src="../images/pdf-doc.svg" alt="" width="120" height="160">
+                <div class="media-body">
+                    <h4 class="mb-3 case-item-title"><a class="fill-area-link" href="">In total</a></h4>
+                    <ul class="list-unstyled list-base mb-0 case-item-list list-base-shift">
+
+                        <li class="my-2">38, 673 predictions</li>
+
+                        <li class="my-2">$38,034 TEAM + 37,952 TEAM tokens won</li>
+
+                        <li class="my-2">126 games played</li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+
+<section>
+    <h3 class="text-uppercase mb-0 text-blue-gray-light">Auctions Module</h3>
+    <div class="row case-row pb-4">
+
+        <div class="col col-4 case-col d-flex">
+            <div class="case-item media my-4 position-relative">
+                <img class="mr-4 case-item-image" src="../images/pdf-doc.svg" alt="" width="120" height="160">
+                <div class="media-body">
+                    <h4 class="mb-3 case-item-title"><a class="fill-area-link" href="">Lorem Ipsum Lorem Ipsum</a></h4>
+                    <ul class="list-unstyled list-base mb-0 case-item-list list-base-shift">
+
+                        <li class="my-2">10,053 predictions</li>
+
+                        <li class="my-2">$38,034 won</li>
+
+                        <li class="my-2">87 games played</li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col col-4 case-col d-flex">
+            <div class="case-item media my-4 position-relative">
+                <img class="mr-4 case-item-image" src="../images/pdf-doc.svg" alt="" width="120" height="160">
+                <div class="media-body">
+                    <h4 class="mb-3 case-item-title"><a class="fill-area-link" href="">Lorem Ipsum Lorem Ipsum</a></h4>
+                    <ul class="list-unstyled list-base mb-0 case-item-list list-base-shift">
+
+                        <li class="my-2">4,323 predictions</li>
+
+                        <li class="my-2">10 428 TEAM tokens won</li>
+
+                        <li class="my-2">13 games played</li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+
+<section>
+    <h3 class="text-uppercase mb-0 text-blue-gray-light">Community Voting Module</h3>
+    <div class="row case-row pb-4">
+
+        <div class="col col-4 case-col d-flex">
+            <div class="case-item media my-4 position-relative">
+                <img class="mr-4 case-item-image" src="../images/pdf-doc.svg" alt="" width="120" height="160">
+                <div class="media-body">
+                    <h4 class="mb-3 case-item-title"><a class="fill-area-link" href="">Lorem Ipsum Lorem Ipsum</a></h4>
+                    <ul class="list-unstyled list-base mb-0 case-item-list list-base-shift">
+
+                        <li class="my-2">10,053 predictions</li>
+
+                        <li class="my-2">$38,034 won</li>
+
+                        <li class="my-2">87 games played</li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col col-4 case-col d-flex">
+            <div class="case-item media my-4 position-relative">
+                <img class="mr-4 case-item-image" src="../images/pdf-doc.svg" alt="" width="120" height="160">
+                <div class="media-body">
+                    <h4 class="mb-3 case-item-title"><a class="fill-area-link" href="">Lorem Ipsum Lorem Ipsum</a></h4>
+                    <ul class="list-unstyled list-base mb-0 case-item-list list-base-shift">
+
+                        <li class="my-2">4,323 predictions</li>
+
+                        <li class="my-2">10 428 TEAM tokens won</li>
+
+                        <li class="my-2">13 games played</li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+
+<section>
+    <h3 class="text-uppercase mb-0 text-blue-gray-light">Scouting Module</h3>
+    <div class="row case-row pb-4">
+
+        <div class="col col-4 case-col d-flex">
+            <div class="case-item media my-4 position-relative">
+                <img class="mr-4 case-item-image" src="../images/pdf-doc.svg" alt="" width="120" height="160">
+                <div class="media-body">
+                    <h4 class="mb-3 case-item-title"><a class="fill-area-link" href="">TokenStars Tennis Scouting</a></h4>
+                    <ul class="list-unstyled list-base mb-0 case-item-list list-base-shift">
+
+                        <li class="my-2">15 athletes in the pipeline</li>
+
+                        <li class="my-2">5 contracts signed with juniors</li>
+
+                        <li class="my-2">Partnerships with 2 academies</li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col col-4 case-col d-flex">
+            <div class="case-item media my-4 position-relative">
+                <img class="mr-4 case-item-image" src="../images/pdf-doc.svg" alt="" width="120" height="160">
+                <div class="media-body">
+                    <h4 class="mb-3 case-item-title"><a class="fill-area-link" href="">TokenStars Football scouting</a></h4>
+                    <ul class="list-unstyled list-base mb-0 case-item-list list-base-shift">
+
+                        <li class="my-2">5 athletes in the pipeline</li>
+
+                        <li class="my-2">2 contracts signed</li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
