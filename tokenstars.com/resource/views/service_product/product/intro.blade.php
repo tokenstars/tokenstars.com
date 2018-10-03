044d2d7bd039894604edf2e00a363be3b06bbd80
Shtepa Stas <shtepa_sy@tokenstars.com>
2018-10-03 11:59:07 +0700

    Intro product

---
+++ tokenstars.com/resource/views/service_product/product/intro.blade.php
@@ -0,0 +1,24 @@
+<div class="jumbotron jumbotron-intro jumbotron-fluid text-white bg-blue-darker position-relative jumbotron-commerce d-flex align-items-center mb-0">
+    <div class="jumbotron-image bg-commerce container"></div>
+    <div class="container jumbotron-container">
+        <div class="row">
+            <div class="col-7">
+                <h1 class="text-uppercase jumbotron-title">
+                    <small class="jumbotron-sup-title text-pink d-block font-weight-bold">Module</small>
+                    E-commerce
+                </h1>
+                <div class="lead jumbotron-lead">
+                    <p>Get exclusive items from TokenStars and our players:</p>
+                    <p>
+                        <ul>
+                            <li style="margin-left: 30px;">Unique merchandise, created in collaboration with TokenStars athletes;</li>
+                            <li style="margin-left: 30px;">Autographed items - equipment, books, etc.;</li>
+                            <li style="margin-left: 30px;">Tournament tickets or locker room passes.</li>
+                        </ul>
+                    </p>
+                    <p>Prices are indicated in the ACE or TEAM tokens.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
