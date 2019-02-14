ae79137f6a5de42e1df160084047b3b08eac3edc
Andrey Mashkovsky <mashkovsky_ab@mail.ru>
2019-02-14 15:33:33 +0700

    - add achievements files;

--- tokenstars.com/resource/views/achievements/admin/achievement_form.blade.php
+++ tokenstars.com/resource/views/achievements/admin/achievement_form.blade.php
@@ -6,6 +6,8 @@
 
 @section('content')
     <script type="text/javascript" src="/js/main-e-commerce.js"></script>
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script src="/vendor/unisharp/laravel-ckeditor/adapters/jquery.js"></script>
     <link rel="stylesheet" href="/css/cropper.css">
     <style>
         img {
@@ -40,7 +42,7 @@
                         @php $bg = 0; @endphp
                         @foreach($intros as $intro)
 
-                            <div class="col-4 border @if($bg) @php $bg = 0; @endphp bg-light @else @php $bg = 1; @endphp bg_white @endif">
+                            <div class="col-6 border @if($bg) @php $bg = 0; @endphp bg-light @else @php $bg = 1; @endphp bg_white @endif">
 
                                 <div class="form-group">
                                     <label class="text-uppercase text-blue-gray-light font-weight-bold"
@@ -62,7 +64,7 @@
                                 <div class="form-group">
                                     <label class="text-uppercase text-blue-gray-light font-weight-bold"
                                            for="description_intro_{{$intro->id}}">Description</label>
-                                    <textarea class="form-control form-control-lg font-weight-bold text-blue-darker"
+                                    <textarea class="form-control form-control-lg font-weight-bold text-blue-darker description-editor"
                                               name="description_intro_{{$intro->id}}" id="" cols="30" rows="3"
                                               maxlength="700"
                                               placeholder="">@if(!empty($intro->description)){{$intro->description}}@endif</textarea>
@@ -91,7 +93,9 @@
                     </div>
                     @php $bg = 0; @endphp
                     @php $bg_color = 'bg-light'; @endphp
+                    @php $cb_count = 0; @endphp
                     @foreach($ahievementModules as $ahievementModule)
+                        @php $cb_count++; @endphp
                         @if($bg)
                             @php
                                 $bg = 0;
@@ -108,7 +112,7 @@
                             <div class="col-6">
                                 <div class="form-group">
                                     <label class="text-uppercase text-blue-gray-light font-weight-bold"
-                                           for="name_module_{{$ahievementModule->id}}">name</label>
+                                           for="name_module_{{$ahievementModule->id}}">name content block № {{$cb_count}}</label>
                                     <input name="name_module_{{$ahievementModule->id}}"
                                            class="form-control form-control-lg font-weight-bold text-blue-darker"
                                            type="text"
@@ -149,10 +153,28 @@
                                         <label class="text-uppercase text-blue-gray-light font-weight-bold"
                                                for="file_achievement_{{$achievement->id}}">file achievement
                                             №{{$j}}</label>
+                                        @if(!empty($achievement->link))
+                                            <div class="text-blue-gray-light mb-2">
+                                                <a href="{{$achievement->link}}">added file</a>
+                                            </div>
+                                        @endif
                                         <input name="file_achievement_{{$achievement->id}}"
                                                class="form-control form-control-lg font-weight-bold text-blue-darker"
-                                               type="file"
-                                               value="">
+                                               type="file">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="text-uppercase text-blue-gray-light font-weight-bold"
+                                               for="image_achievement_{{$achievement->id}}">image achievement
+                                            №{{$j}}</label>
+                                        <div>120px x 160px</div>
+                                        @if(!empty($achievement->image))
+                                            <div class="text-blue-gray-light mb-2">
+                                                <img class="mr-4 case-item-image" src="{{$achievement->image}}" alt="" width="120" height="160">
+                                            </div>
+                                        @endif
+                                        <input name="image_achievement_{{$achievement->id}}"
+                                               class="form-control form-control-lg font-weight-bold text-blue-darker"
+                                               type="file">
                                     </div>
                                     <div class="form-group">
                                         <label class="text-uppercase text-blue-gray-light font-weight-bold"
@@ -162,7 +184,7 @@
                                                type="text"
                                                value="@if(!empty($achievement->weight)){{$achievement->weight}}@endif">
                                     </div>
-                                    @for ($i = 1; $i < 11; $i++)
+                                    @for ($i = 1; $i < 6; $i++)
                                         <div class="form-group">
                                             <label class="text-uppercase text-blue-gray-light font-weight-bold"
                                                    for="item_{{$achievement->id}}_{{$i}}">item {{$i}}</label>
@@ -186,4 +208,7 @@
             </div>
         </fieldset>
     </article>
+    <script type="text/javascript">
+        $('.description-editor').ckeditor();
+    </script>
 @endsection
--- tokenstars.com/resource/views/achievements/sections.blade.php
+++ tokenstars.com/resource/views/achievements/sections.blade.php
@@ -8,11 +8,14 @@
                     @if(strlen($achievement->name) > 0)
                         <div class="col col-4 case-col d-flex">
                             <div class="case-item media my-4 position-relative">
-                                <img class="mr-4 case-item-image" src="../images/pdf-doc.svg" alt="" width="120"
-                                     height="160">
+                                @if($achievement->image)
+                                    <img class="mr-4 case-item-image" src="{{$achievement->image}}" alt="" width="120" height="160">
+                                @else
+                                    <img class="mr-4 case-item-image" src="../images/pdf-doc.svg" alt="" width="120" height="160">
+                                @endif
                                 <div class="media-body">
                                     <h4 class="mb-3 case-item-title"><a class="fill-area-link"
-                                                                        href="">{{$achievement->name}}</a></h4>
+                                                                        href="{{$achievement->link}}" rel="noreferer, ,noopener" target="_blank">{{$achievement->name}}</a></h4>
                                     <ul class="list-unstyled list-base mb-0 case-item-list list-base-shift">
                                         @for ($i = 1; $i < 11; $i++)
                                             @php $item = 'item_'.$i; @endphp
